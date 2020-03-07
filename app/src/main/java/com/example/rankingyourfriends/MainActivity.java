b32c959b1c671ab36597c1cb4f90c2efddc69af0
Jasper <j.bonefaas@student.avans.nl>
2020-03-07 16:08:50 +0100

    Added Question activity

--- app/src/main/java/com/example/rankingyourfriends/MainActivity.java
+++ app/src/main/java/com/example/rankingyourfriends/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.rankingyourfriends;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView jokeTv;
+    private Button playButton;
+    private Button editButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.jokeTv = findViewById(R.id.jokeTextView);
+        this.playButton = findViewById(R.id.playButton);
+        this.editButton = findViewById(R.id.editButton);
+
+        this.playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), Questions.class);
+                view.getContext().startActivity(i);
+            }
+        });
     }
 }
