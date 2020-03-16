dda86b5ba4c8847fe80751d72b6d5f4fbc3c7b7b
Jasper <j.bonefaas@student.avans.nl>
2020-03-16 18:43:32 +0100

    Added MQTT from old project

--- app/src/main/java/com/example/rankingyourfriends/Logic/GameManager.java
+++ app/src/main/java/com/example/rankingyourfriends/Logic/GameManager.java
@@ -1,4 +1,17 @@
 package com.example.rankingyourfriends.Logic;
 
 public class GameManager {
+
+    public static GameManager INSTANCE;
+    private GameManager(){}
+    public static GameManager getInstance(){
+        if(INSTANCE != null){
+            return INSTANCE;
+        }else {
+            INSTANCE = new GameManager();
+            return INSTANCE;
+        }
+    }
+
+
 }
